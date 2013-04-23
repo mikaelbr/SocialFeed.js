129411e985c23424d957783bb060a9249c5e26f4
Mikael Brevik <mikaelbre@gmail.com>
2013-04-23 11:31:57 +0200

    Minor changes to the Youtube module

--- build/socialfeed.js
+++ build/socialfeed.js
@@ -381,99 +381,7 @@ EventEmitter.prototype.listeners = function(type) {
 };
 
 })(require("__browserify_process"))
-},{"__browserify_process":10}],3:[function(require,module,exports){
-var EventEmitter = require('events').EventEmitter
-  , _ = require('./utils')
-  ;
-
-var Controller = module.exports = function ($el) {
-  this.modules = [];
-  this.feedRendered = [];
-
-  this.$el = $el || null;
-
-  this.on('start', _.bind(this.start, this));
-  this.on('reload', _.bind(this.reload));
-  this.on('addModule', _.bind(this.addModule));
-  this.on('synced', _.bind(this.render));
-};
-_.inherits(Controller, EventEmitter);
-
-_.extend(Controller.prototype, {
-  _sync_count: 0
-
-  , addModule: function (module) {
-    this.modules.push(module);
-    this.emit('moduleAdded', module);
-  }
-
-  , start: function () {
-    var controller = this;
-    controller.modules.forEach(function (module) {
-      module.fetch();
-      module.on('fetched', _.bind(controller.moduleFetched, controller));
-      module.on('error', function () { 
-        controller.emit.apply(controller, ['error'].concat(arguments));
-      });
-    });
-  }
-
-  , moduleFetched: function (a, b, c) {
-    if (++this._sync_count === this.modules.length) {
-      // all done
-      this.emit('synced');
-      this.emit('postFetch', this.modules);
-      this._sync_count = 0;
-    }
-  }
-
-  , reload: function () {
-    this.$el.empty();
-    this.emit('preFetch');
-    this.modules.forEach(function (module) {
-      module.fetch();
-    });
-  }
-
-  , render: function () {
-    var $el = this.$el
-      , list = this._generateOrderedList()
-      ;
-
-    list.forEach(function (item) {
-      $el.append(item.html);
-    });
-    this.emit('rendered', list)
-    return this;
-  }
-
-  , _generateOrderedList: function () {
-    var list = [];
-    this.modules.forEach(function (module) {
-      var collectionlist = module.collection.map(function (item) {
-        return {
-          orderBy: module.orderBy(item),
-          html: module.render(item)
-        };
-      });
-      list = list.concat(collectionlist);
-    });
-
-    return this._orderList(list);
-  }
-
-  , _orderList: function (list) {
-    return list.sort(function (x, y) {
-      var a = x.orderBy;
-      var b = y.orderBy;
-      if (a > b || a === void 0) return 1;
-      if (a <= b || b === void 0) return -1;
-    });
-  }
-
-
-});
-},{"events":11,"./utils":5}],4:[function(require,module,exports){
+},{"__browserify_process":10}],4:[function(require,module,exports){
 var EventEmitter = require('events').EventEmitter
   , _ = require('./utils')
   ;
@@ -570,6 +478,98 @@ _.extend(SocialBase.prototype, {
   , orderBy: function (item) {  }
 
   , render: function (item) {  }
+
+});
+},{"events":11,"./utils":5}],3:[function(require,module,exports){
+var EventEmitter = require('events').EventEmitter
+  , _ = require('./utils')
+  ;
+
+var Controller = module.exports = function ($el) {
+  this.modules = [];
+  this.feedRendered = [];
+
+  this.$el = $el || null;
+
+  this.on('start', _.bind(this.start, this));
+  this.on('reload', _.bind(this.reload));
+  this.on('addModule', _.bind(this.addModule));
+  this.on('synced', _.bind(this.render));
+};
+_.inherits(Controller, EventEmitter);
+
+_.extend(Controller.prototype, {
+  _sync_count: 0
+
+  , addModule: function (module) {
+    this.modules.push(module);
+    this.emit('moduleAdded', module);
+  }
+
+  , start: function () {
+    var controller = this;
+    controller.modules.forEach(function (module) {
+      module.fetch();
+      module.on('fetched', _.bind(controller.moduleFetched, controller));
+      module.on('error', function () { 
+        controller.emit.apply(controller, ['error'].concat(arguments));
+      });
+    });
+  }
+
+  , moduleFetched: function (a, b, c) {
+    if (++this._sync_count === this.modules.length) {
+      // all done
+      this.emit('synced');
+      this.emit('postFetch', this.modules);
+      this._sync_count = 0;
+    }
+  }
+
+  , reload: function () {
+    this.$el.empty();
+    this.emit('preFetch');
+    this.modules.forEach(function (module) {
+      module.fetch();
+    });
+  }
+
+  , render: function () {
+    var $el = this.$el
+      , list = this._generateOrderedList()
+      ;
+
+    list.forEach(function (item) {
+      $el.append(item.html);
+    });
+    this.emit('rendered', list)
+    return this;
+  }
+
+  , _generateOrderedList: function () {
+    var list = [];
+    this.modules.forEach(function (module) {
+      var collectionlist = module.collection.map(function (item) {
+        return {
+          orderBy: module.orderBy(item),
+          html: module.render(item)
+        };
+      });
+      list = list.concat(collectionlist);
+    });
+
+    return this._orderList(list);
+  }
+
+  , _orderList: function (list) {
+    return list.sort(function (x, y) {
+      var a = x.orderBy;
+      var b = y.orderBy;
+      if (a > b || a === void 0) return 1;
+      if (a <= b || b === void 0) return -1;
+    });
+  }
+
 
 });
 },{"events":11,"./utils":5}],6:[function(require,module,exports){
@@ -815,7 +815,7 @@ module.exports = {
 	"github_pullrequest": "<div class=\"socialfeed-item socialfeed-github socialfeed-github-pull-request\">\n  <i class=\"socialfeed-icon icon-github\"></i>\n  <header>\n    <h2>\n      <a href=\"{{profileUrl}}\">{{username}}</a> \n      {{action}} pull request <a href=\"{{pullrequesturl}}\">{{pullrequestname}}</a>\n    </h2>\n    <time datetime=\"{{created_at}}\">{{time_since}}</time>\n  </header>\n  <div class=\"socialfeed-body\">\n    {{title}}\n  </div>\n</div>",
 	"github_push": "<div class=\"socialfeed-item socialfeed-github socialfeed-github-push\">\n  <i class=\"socialfeed-icon icon-github\"></i>\n  <header>\n    <h2>\n      <a href=\"{{profileUrl}}\">{{username}}</a> \n      pushed to <a href=\"{{repourl}}\">{{reponame}}</a>\n    </h2>\n    <time datetime=\"{{created_at}}\">{{time_since}}</time>\n  </header>\n  <ul class=\"socialfeed-commit-list\">\n    <li>\n      <a href=\"{{commiturl}}\">{{commit}}</a>\n      <span>{{commit_message}}</span>\n    </li>\n  </ul>\n</div>",
 	"github_watch": "<div class=\"socialfeed-item socialfeed-github socialfeed-github-watch\">\n  <i class=\"socialfeed-icon icon-github\"></i>\n  <header>\n    <h2><a href=\"{{profileUrl}}\">{{username}}</a> starred <a href=\"{{repourl}}\">{{reponame}}</a></h2>\n    <time datetime=\"{{created_at}}\">{{time_since}}</time>\n  </header>\n</div>",
-	"youtubeuploads": "<div class=\"socialfeed-item socialfeed-youtube socialfeed-youtube-upload\">\n  <i class=\"socialfeed-icon icon-youtube\"></i>\n  <header>\n    <h2><a href=\"{{profileurl}}\">{{username}}</a> added a video: <a href=\"{{videourl}}\">{{videoname}}</a></h2>\n    <time datetime=\"{{created_at}}\">{{time_since}}</time>\n  </header>\n  <div class=\"socialfeed-body\">\n    <iframe class=\"youtube-preview\"\n      src=\"http://www.youtube.com/embed/{{entryid}}?wmode=transparent&amp;HD=0&amp;rel=0&amp;showinfo=0&amp;controls=1&amp;autoplay=0\" \n      frameborder=\"0\" \n      allowfullscreen>\n    </iframe>\n    <p>{{desc}}</p>\n  </div>\n</div>",
+	"youtubeuploads": "<div class=\"socialfeed-item socialfeed-youtube socialfeed-youtube-upload\">\n  <i class=\"socialfeed-icon icon-play-circle\"></i>\n  <header>\n    <h2><a href=\"{{profileurl}}\">{{username}}</a> added a video: <a href=\"{{videourl}}\">{{videoname}}</a></h2>\n    <time datetime=\"{{created_at}}\">{{time_since}}</time>\n  </header>\n  <div class=\"socialfeed-body\">\n    <iframe class=\"youtube-preview\"\n      src=\"http://www.youtube.com/embed/{{entryid}}?wmode=transparent&amp;HD=0&amp;rel=0&amp;showinfo=0&amp;controls=1&amp;autoplay=1\" \n      frameborder=\"0\" \n      allowfullscreen>\n    </iframe>\n    <p>{{desc}}</p>\n  </div>\n</div>",
 
 };
 },{}]},{},[1])
